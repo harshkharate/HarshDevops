e66547081996fd4e11f5171a06e16e969a59dbf1
harshkharate <71051015+harshkharate@users.noreply.github.com>
2020-10-17 16:28:37 -0500

    practise revert

--- file2.py
+++ file2.py
@@ -2,4 +2,11 @@ s Hello from harsh
 
 Harsh Loves you 
 All the best for good git learning.
-Go bless you-
+Go bless you.
+
+
+
+All the very best from you .With lots of love and respect
+
+
+We will delete this line and restore it back:
